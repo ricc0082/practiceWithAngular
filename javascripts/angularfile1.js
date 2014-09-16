9cfb2247e68a00bc19a8ca7fc5c9a6221d337f30
ricc0082 <ricc0082@umn.edu>
2014-09-15 20:17:36 -0500

    Placed both apps into one .js file, buttons functioning as intended!!

--- javascripts/angularfile1.js
+++ javascripts/angularfile1.js
@@ -1,29 +1,55 @@
-angular.module('cougarApp', [])
-    .controller('CougarController', ['$scope', function($scope){
-
+angular.module('cougarApp', ['ui.bootstrap'])
+    .controller('CougarController', ['$scope', function($scope) {
         $scope.cougarItems = [
+            {text: 'learn angular', done: true},
+            {text: 'build an angular app', done: false}
+        ];
+
+        $scope.addCougar = function () {
+            $scope.cougarItems.push({text: $scope.todoText, done: false});
+            $scope.todoText = '';
+        };
+
+        $scope.remaining = function () {
+            var count = 0;
+            angular.forEach($scope.cougarItems, function (todo) {
+                count += todo.done ? 0 : 1;
+            });
+            return count;
+        };
+
+        $scope.archive = function () {
+            var oldTodos = $scope.cougarItems;
+            $scope.cougarItems = [];
+            angular.forEach(oldTodos, function (todo) {
+                if (!todo.done) $scope.cougarItems.push(todo);
+            });
+        };
+    }])
+
+    .controller('TodoController', ['$scope', function($scope) {
+        $scope.todos = [
             {text:'learn angular', done:true},
             {text:'build an angular app', done:false}];
 
-        $scope.addCougar = function() {
-            $scope.cougarItems.push({text:$scope.todoText, done:false});
+        $scope.addTodo = function() {
+            $scope.todos.push({text:$scope.todoText, done:false});
             $scope.todoText = '';
         };
 
         $scope.remaining = function() {
             var count = 0;
-            angular.forEach($scope.cougarItems, function(todo) {
+            angular.forEach($scope.todos, function(todo) {
                 count += todo.done ? 0 : 1;
             });
             return count;
         };
 
         $scope.archive = function() {
-            var oldTodos = $scope.cougarItems;
-            $scope.cougarItems = [];
+            var oldTodos = $scope.todos;
+            $scope.todos = [];
             angular.forEach(oldTodos, function(todo) {
-                if (!todo.done) $scope.cougarItems.push(todo);
+                if (!todo.done) $scope.todos.push(todo);
             });
         };
-
     }]);
